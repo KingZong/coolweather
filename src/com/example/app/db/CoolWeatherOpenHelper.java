ab73902ec4a68019dce825346d585203a4867979
King <King@mail.com>
2015-07-25 21:55:49 +0800

    First commit.

---
+++ src/com/example/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,47 @@
+package com.example.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
+
+	//Province表建表语句
+	public static final String CREATE_PROVINCE = "create table Province ( "+
+												"id integer primary key autoincrement, " +
+												"province_name text. " +
+												"province_code text)";;
+	//CIty表建表语句
+	public static final String CREATE_CITY = "create table City (" +
+									"id integer primart key autoincrement, " +
+									"city_name text, " +
+									"city_code text, " +
+									"province_id integer)";		
+	//County表建表语句
+	public static final String CREATE_COUNTY = "create table County (" +
+									"id integer primary ket autoincrement, " +
+									"county_name text, " +
+									"county_code text, " +
+									"city_id integer)";
+												
+	
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
